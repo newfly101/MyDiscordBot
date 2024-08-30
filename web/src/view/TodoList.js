d33816c33e3a64b6c5fc0e2cdb97c9ee4f8f364d
Jae-Hong-Kim <newfly101@naver.com>
2024-08-30 23:39:23 +0900

    [WEB] Todo Category todo List items [data 구조 변경]

--- web/src/view/TodoList.js
+++ web/src/view/TodoList.js
@@ -2,34 +2,29 @@ import React, {useEffect, useRef, useState} from 'react';
 import classes from "../css/Discord.module.css";
 
 const TodoList = () => {
-    // const [isChecked, setIsChecked] = useState(false);
-    // const [addTodoVisible, setAddTodoVisible] = useState(false);
-    // const [todoText, setTodoText] = useState("");
     const [todos, setTodos] = useState([
-        {id: 1, title:"할일 제목1", items: [], isChecked: false, todoText: "", addTodoVisible: false},
-        {id: 2, title:"할일 제목2", items: [], isChecked: false, todoText: "", addTodoVisible: false},
-        {id: 3, title:"할일 제목3", items: [], isChecked: false, todoText: "", addTodoVisible: false}
+        {
+            id: 1,
+            title: "할일 제목1",
+            items: [{id: 1, isChecked: false, todoText: "예시1"}],
+            todoText: '',
+            addTodoVisible: false
+        },
+        {id: 2, title: "할일 제목2", items: [], addTodoVisible: false},
+        {id: 3, title: "할일 제목3", items: [], addTodoVisible: false},
     ]);
     const inputRef = useRef([]);
 
-    const toggleCheckBox = (index, e) => {
+    // todo #Toggle each CheckBox
+    const toggleCheckBox = (index, itemIndex, e) => {
         const newTodos = [...todos];
-        newTodos[index].isChecked = !newTodos[index].isChecked;
+        newTodos[index].items[itemIndex].isChecked = !newTodos[index].items[itemIndex].isChecked;
         setTodos(newTodos);
-        // setIsChecked(!isChecked);
-        // console.log(isChecked);
     }
     const addTodoComponent = (index, e) => {
-        console.log(index);
-        if (!todos[index]) {
-            return ;
-        }
         const newTodos = [...todos];
         newTodos[index].addTodoVisible = true;
         setTodos(newTodos);
-        // if (!addTodoVisible) {
-        //     setAddTodoVisible(true);
-        // }
     }
     const handleInputChange = (index, e) => {
         const newTodos = [...todos];
@@ -38,22 +33,31 @@ const TodoList = () => {
     }
 
     const handleInputSubmit = (index, e) => {
-        if (todos[index].todoText.trim()) {
+        if (todos[index].todoText?.trim()) { // todo #trim() !== 0 이 값을 추가해줌
             const newTodos = [...todos];
-            newTodos[index].items.push(newTodos[index].todoText);
+            const newTodoId = newTodos[index].items.length + 1;
+            newTodos[index].items.push({
+                id: newTodoId,
+                isChecked: false,
+                todoText: newTodos[index].todoText
+            });
+            // todo #초기화 진행
+            newTodos[index].todoText = '';
             newTodos[index].addTodoVisible = false;
-            newTodos[index].todoText = "";
-            // setTodos([...todos, todoText]);
-            // setTodoText('');
-            // setAddTodoVisible(false);
-            console.log("todos : => "+JSON.stringify(todos));
+            // todo #todo 값 설정
+            setTodos(newTodos);
         }
+        console.log("JSON => " + JSON.stringify(todos));
     };
 
     useEffect(() => {
-        const handleClickOutside = (index, e) => {
+        // todo #input창 생성 시 focus 주기 필요함(편의성** )
+        const handleClickOutside = (e) => {
             const newTodos = [...todos];
-            newTodos.forEach((todo) => {
+            newTodos.forEach((todo, index) => {
+                if (todo.addTodoVisible && inputRef.current[index]) {
+                    inputRef.current[index].focus();
+                }
                 if (inputRef.current[index] && !inputRef.current[index].contains(e.target)) {
                     handleInputSubmit(index, e);
                     newTodos[index].addTodoVisible = false;
@@ -61,76 +65,47 @@ const TodoList = () => {
                 }
             });
             setTodos(newTodos);
-            // if (inputRef.current && !inputRef.current.contains(event.target)) {
-            //     handleInputSubmit(event);
-            //     setAddTodoVisible(false);
-            //     setTodoText('');
-            // }
         };
         document.addEventListener('mousedown', handleClickOutside);
         return () => {
             document.removeEventListener('mousedown', handleClickOutside);
         };
     }, [todos]);
-    // }, [inputRef, todoText]);
 
     return (
         <div className={classes.test}>
-            <article>
-                {todos.map((todo, index) => (
-                    <div key={todo.id}>
-                        <button className={classes.todoTemplate} data-icon="alone"
-                                onClick={(e) => addTodoComponent(index, e)}>{todo.title}</button>
-                        {todo.items.map((item, itemIndex) => (
-                            <div key={itemIndex} className={classes.addTodo}>
-                                <span className={`${classes.checkbox} ${todo.isChecked ? classes.active : ''}`}
-                                      onClick={(e) => toggleCheckBox(index, e)}/>
-                                <label>{item}</label>
-                            </div>
-                        ))}
+            {/*todo #todos[] 가 존재 하면  title 출력*/}
+            {todos.map((todo, index) => (
+                <article key={index}>
+                    <button className={classes.todoTemplate} data-icon="alone"
+                            onClick={(e) => addTodoComponent(index, e)}>{todo.title}</button>
+                    {/* todo 2. todos[index].items 가 있으면 내용(label) 출력 */}
+                    {todo.items && todo.items.map((item, itemIndex) => (
+                        <div key={item.id} className={classes.addTodo}>
+                                <span className={`${classes.checkbox} ${item.isChecked ? classes.active : ''}`}
+                                      onClick={(e) => toggleCheckBox(index, itemIndex, e)}/>
+                            <label>{item.todoText}</label>
+                        </div>
+                    ))}
 
-                        {todo.addTodoVisible && (
-                            <div className={classes.addTodo}  ref={(el) => (inputRef.current[index] = el)}>
+                    {/*todo #addTodoComponent() 동작 이후 input 창 추가되어지는 부분*/}
+                    {todo.addTodoVisible && (
+                        <div className={classes.addTodo} ref={(el) => (inputRef.current[index] = el)}>
                             <span className={`${classes.checkbox} ${todo.isChecked ? classes.active : ''}`}
                                   onClick={toggleCheckBox}/>
-                                <input
-                                    type='text'
-                                    placeholder="할 일 입력"
-                                    value={todo.todoText}
-                                    onChange={(e) => handleInputChange(index, e)}
-                                    onKeyDown={(e) => {
-                                        e.key === 'Enter' && handleInputSubmit(index, e);
-                                    }}
-                                />
-                            </div>
-                        )}
-                    </div>
-                ))}
-                {/*<button className={classes.todoTemplate} data-icon="alone"*/}
-                {/*        onClick={addTodoComponent}>&#36;&#123;할일제목&#125;</button>*/}
-                {/*{todos.map((todo, index) => (*/}
-                {/*    <div className={classes.addTodo}>*/}
-                {/*            <span className={`${classes.checkbox} ${isChecked ? classes.active : ''}`}*/}
-                {/*                  onClick={toggleCheckBox}/>*/}
-                {/*        <label>{todo}</label>*/}
-                {/*    </div>*/}
-                {/*))}*/}
-                {/*{addTodoVisible && (*/}
-                {/*    <div className={classes.addTodo} ref={inputRef}>*/}
-                {/*            <span className={`${classes.checkbox} ${isChecked ? classes.active : ''}`}*/}
-                {/*                  onClick={toggleCheckBox}/>*/}
-                {/*        <input*/}
-                {/*            type='text'*/}
-                {/*            placeholder="할 일 입력"*/}
-                {/*            value={todoText}*/}
-                {/*            onChange={handleInputChange}*/}
-                {/*            onKeyDown={(e) => {*/}
-                {/*                e.key === 'Enter' && handleInputSubmit(e);*/}
-                {/*            }}*/}
-                {/*        />*/}
-                {/*    </div>*/}
-                {/*)}*/}
-            </article>
+                            <input
+                                type='text'
+                                placeholder="할 일 입력"
+                                value={todo.todoText}
+                                onChange={(e) => handleInputChange(index, e)}
+                                onKeyDown={(e) => {
+                                    e.key === 'Enter' && handleInputSubmit(index, e);
+                                }}
+                            />
+                        </div>
+                    )}
+                </article>
+            ))}
         </div>
     );
 };
